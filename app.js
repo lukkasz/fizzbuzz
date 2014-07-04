e24b87e3ed29d477628617c67e10ed133b253640
ph0rest82 <ph0rest82@gmail.com>
2014-07-04 15:14:19 +0200

    Added function fizzBuzz

--- app.js
+++ app.js
@@ -3,19 +3,27 @@ $(document).ready(function(){
 	$('.btn').on('click', function(){
 
 		var number = $('#number').val();
-		var rez = $('.result');
-		rez.empty();
-		for (var i = 1; i <= number; i++) {
-			if ( ( i%3 == 0 ) && ( i%5 == 0 ) ) {
-				rez.append("<span>FizzBuzz, </span>");
-			} else if (i%3 == 0) {
-				rez.append("<span>Fizz, </span>");
-			} else if (i%5 == 0) {
-				rez.append("<span>Buzz, </span>");
-			} else {
-				rez.append("<span>" + i + ", </span>")
+		
+		
+		var fizzBuzz = function ( num ) {
+			var rez = $('.result');
+			rez.empty();
+			for (var i = 1; i <= num; i++) {
+				if ( ( i%3 == 0 ) && ( i%5 == 0 ) ) {
+					rez.append("<span>FizzBuzz, </span>");
+				} else if (i%3 == 0) {
+					rez.append("<span>Fizz, </span>");
+				} else if (i%5 == 0) {
+					rez.append("<span>Buzz, </span>");
+				} else {
+					rez.append("<span>" + i + ", </span>")
+				}
 			}
-		}
+			return rez;
+		};
+
+		fizzBuzz(number);
+		
 		
 	});
 	
